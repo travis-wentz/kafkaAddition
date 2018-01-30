07850b75e32f6267c423b6761c06dea23cb25a84
Tech <tech@Tech-MacBook-Pro.local>
2018-01-30 15:53:24 -0700

    updates AdditionController (forgot to save)

--- src/main/java/addition/AdditionController.java
+++ src/main/java/addition/AdditionController.java
@@ -6,9 +6,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AdditionController {
+	private int intToAdd;
 
 	@RequestMapping("/calculator/add")
 	public String add(@RequestParam(value="value", defaultValue="0") int value) {
+		intToAdd = value;
 		return "A value of " + value + " was added.";
 	}
 }
