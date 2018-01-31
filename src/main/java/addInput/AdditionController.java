60a0e3987d0946494da9750a2eb4b4dc015e08e8
Tech <tech@Tech-MacBook-Pro.local>
2018-01-31 15:34:30 -0700

    moves input producer functionality from AdditionController to ProduceInput

--- src/main/java/addInput/AdditionController.java
+++ src/main/java/addInput/AdditionController.java
@@ -1,9 +1,5 @@
 package addInput;
 
-import org.apache.kafka.clients.producer.*;
-import org.apache.kafka.common.serialization.LongSerializer;
-import org.apache.kafka.common.serialization.StringSerializer;
-import java.util.Properties;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AdditionController {
 	private final static String TOPIC = "input";
-	private final static String BOOTSTRAP_SERVERS = "localhost:2181";
 
 	/**
 	 * Gets the int value given in the URL.
@@ -26,42 +21,7 @@ public class AdditionController {
 	 */
 	@RequestMapping("/calculator/add")
 	public String add(@RequestParam(value = "value", defaultValue = "0") int value) throws Exception {
-		if(runProducer(value)) { return ("A value of " + value + " was added to " + TOPIC + " topic."); }
+		if(ProduceInput.runProducer(value)) { return ("A value of " + value + " was added to " + TOPIC + " topic."); }
 		return "Something went wrong. No value was added.";
-	}
-
-	/**
-	 * Takes in an int value, creates a producer, adds the value to the producer.
-	 * @param valueToAdd
-	 * @return success or failure boolean
-	 * @throws Exception
-	 */
-	private static boolean runProducer(final int valueToAdd) throws Exception {
-		final Producer<Long, String> producer = createProducer();
-		boolean success = false;
-		
-		try { //try to create a record and send the record to producer
-			final ProducerRecord<Long, String> record = new ProducerRecord<>(TOPIC, Integer.toString(valueToAdd));
-			producer.send(record);
-			success = true;
-		} finally {
-			producer.flush();
-			producer.close();
-		}
-		
-		return success;
-	}
-
-	/**
-	 * Creates a kafka producer.
-	 * @return Producer
-	 */
-	private static Producer<Long, String> createProducer() {
-		Properties props = new Properties();
-		props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
-		props.put(ProducerConfig.CLIENT_ID_CONFIG, "KafkaExampleProducer");
-		props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, LongSerializer.class.getName());
-		props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-		return new KafkaProducer<>(props);
 	}
 }
